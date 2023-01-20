c7a76d4d0a74f34bb0b47ec9c6757a6389a7404f
jmagno <j@magno.io>
2023-01-19 22:37:22 -0600

    Backing it Up

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -7,7 +7,8 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
+let firstItem = arr[0]
+console.log(firstItem)
 
 ////////// PROBLEM 2 //////////
 /*
@@ -15,6 +16,10 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+lastItemRemoved = arr.pop();
+console.log(lastItemRemoved);
+console.log(arr)
+
 
 ////////// PROBLEM 3 //////////
 
@@ -27,7 +32,8 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for(i  = 0; i < family.length; i++){
+  console.log(family[i])
 
 
 ////////// PROBLEM 4 //////////
@@ -42,7 +48,12 @@ let evensArr = []
 */
 
 //Code Here
-
+for (i = 0; i < nums.length; i++){
+  if(nums[i] % 2 === 0 ){
+    evensArr.push(nums[i]);
+  }
+}
+console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
@@ -56,7 +67,17 @@ var score = 74
 */
 
 //Code Here
-
+if (score >= 90){
+  console.log('A')
+} else if (score >= 80){
+  console.log('B')
+} else if (score >= 70){
+  console.log('C')
+} else if (score >= 60){
+  console.log('D')
+} else {
+  console.log('F')
+}
 
 ////////// Intermediate Problems //////////
 
@@ -71,7 +92,8 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
-
+let someNum = myFavoriteNumbers[4]
+console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
@@ -79,7 +101,12 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-
+if (myFavoriteNumbers.length < 7){
+  console.log('There are not enough elements in this array.')
+} else {
+  someNum = myFavoriteNumbers[6]
+}
+console.log(someNum)
 
 ////////// PROBLEM 8 //////////
 
@@ -90,7 +117,13 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for (i = 0; i < listOfNumbers.length; i++){
+  if (listOfNumbers[i] % 3 === 0) {
+    console.log(`${listOfNumbers[i]} is divisible by 3.`)
+  } else {
+    console.log(`${listOfNumbers[i]} is NOT divisible by 3 lol`)
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -102,7 +135,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (i = letters.length - 1; i >=0; i--){
+  console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
@@ -124,7 +159,26 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch (letterGrade) {
+  case "A":
+    console.log('"The student is doing excellently."');
+    break;
+  case "B":
+    console.log('"The student is doing well."');
+    break;
+  case "C":
+    console.log('"The student is doing okayyyy."');
+    break;
+  case "D":
+    console.log('"The student is not doing bery well."');
+    break;
+  case "F":
+    console.log('"The student is failing but still smarter than mango"');
+    break;
+  default:
+    console.log('"Not an eligable grade"')
+    break;
+}
 
 
 ////////// PROBLEM 11 //////////
