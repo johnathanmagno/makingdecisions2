87b385b8f68ea5ac0ad365ce4dd0b45eed914a01
jmagno <j@magno.io>
2023-01-19 22:46:46 -0600

    Finished Making Decisions pt 2

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -206,3 +206,11 @@ switch (letterGrade) {
 */
 
 //Code Here
+for (let i = 1; i <= 100; i++){
+  if (i % 15 === 0) console.log(`DevMountain`);
+  else if (i % 3 === 0) console.log(`Dev`);
+  else if (i % 5 === 0) console.log('Mountain')
+  else console.log(i)
+}
+
+//DONE
